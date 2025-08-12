6abf256ad0ca76dd155401721ca146b84dcf7001
Mahmoudashraf213 <mhmwdbhjt307@gmail.com>
2025-08-13 02:26:58 +0300

    first commit

---
+++ src/bootStrap.js
@@ -0,0 +1,20 @@
+import cors from 'cors';
+import { globalErrorHandling } from "./utils/appError.js";
+
+export const bootStrap = (app, express) => {
+    // parse req
+    app.use(express.json());
+    // cors edit
+    const corsOptions = {
+        origin: '*',
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        credentials: true,
+    };
+    app.use(cors(corsOptions));
+
+    // routing
+
+
+    // global error
+    app.use(globalErrorHandling);
+};
